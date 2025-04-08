a0d18d49ca84d2536ae0b0644d7b320ea010cd80
joshua <jozzydgreat1@gmail.com>
2025-04-08 09:20:06 -0700

    fixed header write up

--- src/Pages/Component/Home/Header.tsx
+++ src/Pages/Component/Home/Header.tsx
@@ -34,10 +34,10 @@ export const Header = ()=>{
             <div style={styles.content}>
                 <div className='headeritem' style={{...styles.item, flexDirection:'column',justifyContent:'center'}}>
                     <h1>
-                    Get Quality Palm Oil Online in Nigeria – Fresh, Pure & Affordable
+                    Get Quality Palm Oil Delivered Worldwide – Fresh, Pure & Affordable
                     </h1>
                     <p>
-                    Get Quality Palm Oil Delivered Worldwide – Fresh, Pure & Affordable
+                   
                     Looking for where to get palm oil you can trust? Whether you need a single bottle or bulk supply, 
                     we deliver fresh and premium palm oil straight from the farm to your doorstep — rich in taste and perfect for home or commercial use, anywhere in the world.
                     </p>
